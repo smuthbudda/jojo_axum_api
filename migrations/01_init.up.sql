4c6e947a5934afa28e20950f5b7cd0024033fe16
Jordan Samson <44793715+smuthbudda@users.noreply.github.com>
2024-08-09 20:03:13 +1000

    dwad

--- migrations/01_init.up.sql
+++ migrations/01_init.up.sql
@@ -1,18 +1,13 @@
-CREATE TABLE IF NOT EXISTS todos (
-    id SERIAL PRIMARY KEY NOT NULL,
-    text VARCHAR(255) NOT NULL,
-    done BOOLEAN DEFAULT false NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_name VARCHAR(255) NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    phone VARCHAR(20),
-    active BOOLEAN DEFAULT false,
-    password VARCHAR(255)
+    user_name VARCHAR NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    phone VARCHAR,
+    active BOOLEAN,
+    date_modified TIMESTAMP DEFAULT NULL,
+    password VARCHAR
 );
 
 CREATE TABLE IF NOT EXISTS points (
@@ -21,17 +16,19 @@ CREATE TABLE IF NOT EXISTS points (
     gender VARCHAR(10),
     category VARCHAR(20),
     event VARCHAR(20),
-    mark FLOAT
+    mark FLOAT,
+    mark_time TIME DEFAULT '00:00:00'
 );
 
-CREATE TABLE IF NOT EXISTS user_pages (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID REFERENCES users(id) NOT NULL
+CREATE TABLE IF NOT EXISTS user_points (
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    point_id INTEGER REFERENCES points(id) ON DELETE CASCADE,
+    date_achieved TIMESTAMP DEFAULT NULL,
+    PRIMARY KEY (user_id, point_id)
 );
 
-CREATE TABLE IF NOT EXISTS page_template (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_page_id UUID REFERENCES user_pages(id) NOT NULL,
-    page_title VARCHAR(100),
-    page_body VARCHAR(10000)
+CREATE TABLE IF NOT EXISTS todos (
+    id SERIAL PRIMARY KEY NOT NULL,
+    text VARCHAR(255) NOT NULL,
+    done BOOLEAN DEFAULT false NOT NULL
 );
